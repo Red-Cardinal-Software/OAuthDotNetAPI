56f14c551f00e0da9a39c6bd64a2062ea936998e
James Harrold <james@redcardinalsoftware.com>
2025-08-08 11:28:47 -0500

    Adds azure key vault code and authentication
    Commented out block for azure key vault implementation if needed.
    JWT are now properly read and validated on requests

--- OAuthDotNetAPI/WebApi/Program.cs
+++ OAuthDotNetAPI/WebApi/Program.cs
@@ -1,4 +1,7 @@
+using Azure.Identity;
 using DependencyInjectionConfiguration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,8 +9,37 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+/**
+// Uncomment this section if using Azure Key Vault
+if (builder.Environment.IsProduction())
+{
+    var keyVaultName = builder.Configuration["KeyVaultName"];
+    if (!string.IsNullOrEmpty(keyVaultName))
+    {
+        var keyVaultUri = new Uri($"https://{keyVaultName}.vault.azure.net/");
+        builder.Configuration.AddAzureKeyVault(keyVaultUri, new DefaultAzureCredential());
+    }
+}
+*/
+
 builder.Services.AddAppDependencies(builder.Environment);
 builder.Services.AddControllers();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey =
+                new SymmetricSecurityKey(
+                    System.Text.Encoding.UTF8.GetBytes(builder.Configuration["AppSettings-Token"]!)),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ClockSkew = TimeSpan.Zero
+        };
+    });
 
 var app = builder.Build();
 
